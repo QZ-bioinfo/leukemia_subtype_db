53af441a9c70d403c3e2100e41a1bb456f8d18c8
发明家 <1804678189@qq.com>
2023-01-04 21:21:30 +0800

    leukemia第三次，新增差异查询

--- app.py
+++ app.py
@@ -6,29 +6,56 @@ app = Flask(__name__)
 
 search_tool = db_search()
 
+@app.route('/')
+def return_blank():
+    return ''
+
 @app.route('/total')
 def database_total():
     return search_tool.get_total_()
 
 @app.route('/search',methods=["GET"])
 def database_search():
-    data_type = request.args.get("data_type", type=str, default="lnc")
-    cell_type = request.args.get("cell_type", type=str, default="AML")
-    lnc_id = request.args.get("lnc_id", type=str, default="NONHSAG058641.2")
-    gene_name = request.args.get("gene_name", type=str, default="ZBP1")
-    if data_type == "lnc":
+    data_type = request.args.get('data_type', type=str, default='lnc')
+    cell_type = request.args.get('cell_type', type=str, default='AML')
+    lnc_id = request.args.get('lnc_id', type=str, default='NONHSAG058641.2')
+    gene_name = request.args.get('gene_name', type=str, default='ZBP1')
+    if data_type == 'lnc':
         for key in search_tool.lnc_pc_list:
             if cell_type == key:
                 return search_tool.get_data_({'data_type':'lnc','cell_type':key,'id':lnc_id})
-    if data_type == "Protein-Coding-RNA":
+    if data_type == 'Protein-Coding-RNA':
         for key in search_tool.lnc_pc_list:
             if cell_type == key:
                 return search_tool.get_data_({'data_type':'pc','cell_type':key,'gene_name':gene_name})
-    if data_type == "Alternative-Splicing":
+    if data_type == 'Alternative-Splicing':
         for key in search_tool.as_list:
             if cell_type == key:
                 return search_tool.get_data_({'data_type':'as','cell_type':key,'gene_name':gene_name})
-    return '{}'
+    return "{'data':[],total:0}"
+
+@app.route('/diff',methods=["GET"])
+def database_diff():
+    cell_type = request.args.get('cell_type', type=str)
+    lnc_id = request.args.get('lnc_id', type=str, default='NONHSAG058641.2')
+    try:
+        cell_type = re.search('\[(.*)\]',cell_type).group(1)
+        cell_type_list = cell_type.replace(' ','').split(',')
+        for i in range(len(cell_type_list)):
+            if cell_type_list[i] != '':
+                cell_type_list[i] = cell_type_list[i].replace(cell_type_list[i][0],'')
+        print(cell_type_list)
+        temp_list = []
+        for i in cell_type_list:
+            for key in search_tool.lnc_pc_list:
+                if i == key:
+                    temp_list.append(key)
+                    continue
+        if len(temp_list) == 0:
+            return '{}'
+        return search_tool.get_lnc_diff({'cell_type':temp_list,'id':lnc_id})
+    except:
+        return "{}"
 
 if __name__ == "__main__":
     app.run(host='localhost',port = 8080)
--- db_search.py
+++ db_search.py
@@ -3,75 +3,76 @@ import json
 
 client = pymongo.MongoClient('')
 
-AML = client["AML"]
-AML_lnc = AML["lnc"]
-AML_pc = AML["Protein-coding"]
+AML = client['AML']
+AML_lnc = AML['lnc']
+AML_pc = AML['Protein-coding']
 
-APL = client["APL"]
-APL_lnc = APL["lnc"]
-APL_as = APL["alternative-splicing"]
-APL_pc = APL["Protein-coding"]
+APL = client['APL']
+APL_lnc = APL['lnc']
+APL_as = APL['alternative-splicing']
+APL_pc = APL['Protein-coding']
 
-BcellAll = client["B-Cell-ALL"]
-BcellAll_lnc = BcellAll["lnc"]
-BcellAll_pc = BcellAll["Protein-coding"]
+BcellAll = client['B-Cell-ALL']
+BcellAll_lnc = BcellAll['lnc']
+BcellAll_pc = BcellAll['Protein-coding']
 
-CLL = client["CLL"]
-CLL_lnc = CLL["lnc"]
-CLL_as = CLL["alternative-splicing"]
-CLL_pc = CLL["Protein-coding"]
+CLL = client['CLL']
+CLL_lnc = CLL['lnc']
+CLL_as = CLL['alternative-splicing']
+CLL_pc = CLL['Protein-coding']
 
-CML = client["CML"]
-CML_lnc = CML["lnc"]
-CML_pc = CML["Protein-coding"]
+CML = client['CML']
+CML_lnc = CML['lnc']
+CML_pc = CML['Protein-coding']
 
-MDS = client["MDS"]
-MDS_lnc = MDS["lnc"]
-MDS_pc = MDS["Protein-coding"]
+MDS = client['MDS']
+MDS_lnc = MDS['lnc']
+MDS_pc = MDS['Protein-coding']
 
-PreBCellALL = client["Pre-B-Cell-ALL"]
-PreBCellALL_lnc = PreBCellALL["lnc"]
-PreBCellALL_pc = PreBCellALL["Protein-coding"]
+PreBCellALL = client['Pre-B-Cell-ALL']
+PreBCellALL_lnc = PreBCellALL['lnc']
+PreBCellALL_pc = PreBCellALL['Protein-coding']
 
-TcellAll = client["T-Cell-ALL"]
-TcellAll_lnc = TcellAll["lnc"]
-TcellAll_as = TcellAll["alternative-splicing"]
-TcellAll_pc = TcellAll["Protein-coding"]
+TcellAll = client['T-Cell-ALL']
+TcellAll_lnc = TcellAll['lnc']
+TcellAll_as = TcellAll['alternative-splicing']
+TcellAll_pc = TcellAll['Protein-coding']
 
-aCML = client["aCML"]
-aCML_lnc = aCML["lnc"]
-aCML_pc = aCML["Protein-coding"]
-aCML_as = aCML["alternative-splicing"]
+aCML = client['aCML']
+aCML_lnc = aCML['lnc']
+aCML_pc = aCML['Protein-coding']
+aCML_as = aCML['alternative-splicing']
 
+Normal_white_cell = client['Normal-white-cell']
+Normal_white_cell_lnc = Normal_white_cell['lnc']
     
 lnc_dic = {'AML':AML_lnc,'APL':APL_lnc,'B-Cell-ALL':BcellAll_lnc,'CLL':CLL_lnc,'CML':CML_lnc,'MDS':MDS_lnc,'Pre-B-Cell-ALL':PreBCellALL_lnc,'T-Cell-ALL':TcellAll_lnc,'aCML':aCML_lnc}
 pc_dic = {'AML':AML_pc,'APL':APL_pc,'B-Cell-ALL':BcellAll_pc,'CLL':CLL_pc,'CML':CML_pc,'MDS':MDS_pc,'Pre-B-Cell-ALL':PreBCellALL_pc,'T-Cell-ALL':TcellAll_pc,'aCML':aCML_pc}
 as_dic = {'APL':APL_as,'CLL':CLL_as,'T-Cell-ALL':TcellAll_as,'aCML':aCML_as}
-search_dic = {"lnc":lnc_dic,"pc":pc_dic,"as":as_dic}
+search_dic = {'lnc':lnc_dic,'pc':pc_dic,'as':as_dic}
 
 class db_search(object):
 
-    lnc_pc_list = ["AML","APL","B-Cell-ALL","CLL","CML","MDS","Pre-B-Cell-ALL","T-Cell-ALL","aCML"]
-    as_list = ["APL","CLL","T-Cell-ALL","aCML"]
+    lnc_pc_list = ['AML','APL','B-Cell-ALL','CLL','CML','MDS','Pre-B-Cell-ALL','T-Cell-ALL','aCML']
+    as_list = ['APL','CLL','T-Cell-ALL','aCML']
 
     def get_data_(self,dic):
-        return_dic = {}
-        num = 1
+        return_dic = {'data':[]}
         if dic['data_type'] == 'as':
             for single_record in search_dic[dic['data_type']][dic['cell_type']].find({'geneSymbol':dic['gene_name']}):
                 single_record.pop('_id')
-                return_dic[num] = single_record
-                num += 1
+                return_dic['data'].append(single_record)
         if dic['data_type'] == 'lnc':
             for single_record in search_dic[dic['data_type']][dic['cell_type']].find({'id':dic['id']}):
                 single_record.pop('_id')
-                return_dic[num] = single_record
-                num += 1
+                single_record['Mean_exp'] = float(single_record['Mean_exp'])
+                return_dic['data'].append(single_record)
         if dic['data_type'] == 'pc':
             for single_record in search_dic[dic['data_type']][dic['cell_type']].find({'gene_name':dic['gene_name']}):
                 single_record.pop('_id')
-                return_dic[num] = single_record
-                num += 1
+                single_record['Mean_exp'] = float(single_record['Mean_exp'])
+                return_dic['data'].append(single_record)
+        return_dic['total'] = len(return_dic['data'])
         return str(json.dumps(return_dic))
 
     def get_total_(self):
@@ -85,4 +86,20 @@ class db_search(object):
             return_dic['Protein-Coding-RNA'][key] = value.count_documents({})
         for key,value in as_dic.items(): 
             return_dic['alternative-splicing'][key] = value.count_documents({})
+        return str(json.dumps(return_dic))
+
+    def get_lnc_diff(self,dic):
+        if Normal_white_cell_lnc.find_one({'id':dic['id']}) == None:
+            return '{}'
+        return_dic = {'Normal_white_cell':[]}
+        for every_cell_type in dic['cell_type']:
+            return_dic[every_cell_type] = []
+            for single_record in lnc_dic[every_cell_type].find({'id':dic['id']}):
+                single_record.pop('_id')
+                single_record['Mean_exp'] = float(single_record['Mean_exp'])
+                return_dic[every_cell_type].append(single_record)
+        for single_record in Normal_white_cell_lnc.find({'id':dic['id']}):
+            single_record.pop('_id')
+            single_record['Mean_exp'] = float(single_record['Mean_exp'])
+            return_dic['Normal_white_cell'].append(single_record)
         return str(json.dumps(return_dic))
